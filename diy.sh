12c7e1eb273a05969eef9f7ced5409fd0ab8124c
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-05 17:22:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,9 @@
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
-git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean/
 rm -rf package/lean/luci-lib-docker package/lean/luci-app-dockerman package/lean/luci-theme-argon package/lean/luci-theme-netgear package/lean/frpc package/lean/luci-app-frpc
 cp ../mac80211.sh package/kernel/mac80211/files/lib/wifi/
+cp ../Makefile package/luci-app-openclash/
