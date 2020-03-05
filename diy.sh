129ec3665d410d41313abeff50141207fe81cd14
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-05 13:41:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
