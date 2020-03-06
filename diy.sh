b171269cfa55f8014ec9c536b559e3187f0f1e39
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-06 19:39:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 #git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
-git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean/
 #rm -rf package/lean/luci-lib-docker package/lean/luci-app-dockerman package/lean/luci-theme-argon package/lean/luci-theme-netgear package/lean/frpc package/lean/luci-app-frpc
@@ -18,3 +18,5 @@ sed -i 's/VHT80/VHT40/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/\..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/' package/luci-app-smartdns/Makefile
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/autosamba
