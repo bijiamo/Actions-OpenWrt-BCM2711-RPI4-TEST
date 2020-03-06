6a350b82039afd5af0b0a8c55931678d63a0670f
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-07 05:29:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,7 @@
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
 #svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/autosamba
+#svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
@@ -21,7 +22,7 @@ git clone https://github.com/honwen/luci-app-shadowsocks package/luci-app-shadow
 git clone https://github.com/honwen/luci-app-shadowsocksr package/luci-app-shadowsocksr
 git clone https://github.com/kuoruan/luci-app-v2ray package/luci-app-v2ray
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 sed -i 's/VHT80/VHT40/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/\..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/' package/luci-app-smartdns/Makefile
