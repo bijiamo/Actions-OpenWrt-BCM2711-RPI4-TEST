0186a67bc76d65851a3edb1c33029a76464e7d14
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-06 15:25:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,14 +8,15 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean/
-rm -rf package/lean/luci-lib-docker package/lean/luci-app-dockerman package/lean/luci-theme-argon package/lean/luci-theme-netgear package/lean/frpc package/lean/luci-app-frpc
-cp ../mac80211.sh package/kernel/mac80211/files/lib/wifi/
-cp ../Makefile package/luci-app-openclash/
-git pull
-./scripts/feeds update -a
-./scripts/feeds install -a
+#svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean/
+#rm -rf package/lean/luci-lib-docker package/lean/luci-app-dockerman package/lean/luci-theme-argon package/lean/luci-theme-netgear package/lean/frpc package/lean/luci-app-frpc
+sed -i 's/VHT80/VHT40/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#cp ../Makefile package/luci-app-openclash/
+#git pull
+#./scripts/feeds update -a
+#./scripts/feeds install -a
